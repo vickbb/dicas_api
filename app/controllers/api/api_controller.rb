f1a22be3b720b910f30ba285b5d2713887898a6f
Victor Barreiros <victor_barreiros@hotmail.com>
2019-11-22 14:05:17 -0300

    Removido autentificação da api

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,15 +1,4 @@
 module Api
   class ApiController < ActionController::Base
-    # before_action :restrict_access
-
-    private
-      def restrict_access
-        unless Rails.env.development?
-          authenticate_or_request_with_http_token do |token, options|
-            ApiKey.exists?(access_token: token)
-          end
-        end
-      end
-      
   end
 end
